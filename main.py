c7aaedf7deac1bc76509ca40fdc4a5326e8b2dcf
emkeszler <emkeszler@gmail.com>
2025-03-25 21:04:38 -0400

    added game looping

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from constants import *
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print("Starting Asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
-	while:
+	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		fill(black)
-		display.flip()
+		screen.fill("black")
+		pygame.display.flip()
 
 if __name__ == "__main__":
     main()
